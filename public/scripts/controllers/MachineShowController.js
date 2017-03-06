0ba2ee6fdec8f5ff10ca98d7de873959f04906a9
Will Kaspar <Will@Aang.local>
2017-03-05 21:40:38 -0800

    submit button tied together, something about CORS headers, also pinballmaps just changed their API. yay.

--- public/scripts/controllers/MachineShowController.js
+++ public/scripts/controllers/MachineShowController.js
@@ -15,22 +15,6 @@ function MachineShowController($http, $routeParams, $location){
   var gameId = parseInt($routeParams.id);
   vm.dataLoaded = false;
 
-  // GET MACHINE BY ID TO POPULATE PAGE //
-  $http({
-    method: 'GET',
-    url: 'http://pinballmap.com/api/v1/locations/2405/machine_details.json'
-  }).then(function onSuccess(res){
-    vm.machineList = res.data.machines;
-    vm.machineList.forEach(function(machine){
-      if (machine.id == gameId)
-        vm.machine = machine;
-    });
-    console.log('current pin: ', vm.machine);
-
-  }, function onError(res){
-    console.log('failjax: ', res);
-  }); // closes first $http
-
 
   // GET MACHINE ID FOR HIGH SCORES //
   console.log('gameId: ', gameId);
@@ -41,34 +25,68 @@ function MachineShowController($http, $routeParams, $location){
     var data = res.data.location_machine_xrefs;
     data.forEach(function(pin){
       if((pin.location_id === 2405) && (pin.machine.id === gameId)){
-        vm.pin = pin.id;
+        vm.pin = pin;
       }
     }); // closes data.forEach
     console.log('current pin from all of bay: ', vm.pin.id);
 
+
+
+  // // GET MACHINE BY ID TO POPULATE PAGE //
+  // $http({
+  //   method: 'GET',
+  //   url: 'http://pinballmap.com/api/v1/locations/2405/machine_details.json'
+  // }).then(function onSuccess(res){
+  //   vm.machineList = res.data.machines;
+  //   vm.machineList.forEach(function(machine){
+  //     if (machine.id == gameId)
+  //       vm.machine = machine;
+  //   });
+  //   //console.log('current pin: ', vm.machine);
+  // }, function onError(res){
+  //   //console.log('failjax: ', res);
+  // }); // closes first $http
+
+
+
+
     // GET HIGH SCORES BY MACHINE ID //
     $http({
       method: 'GET',
-      url: 'http://pinballmap.com/api/v1/machine_score_xrefs/'+ vm.pin + '.json'
+      url: 'http://pinballmap.com/api/v1/machine_score_xrefs/'+ vm.pin.id + '.json'
     }).then(function onSuccess(res){
       console.log('highscoresjax', res.data);
       vm.highscores = res.data.machine_scores;
       vm.dataLoaded = true;
-
-
     }, function onError(res){
       console.log('hsfailsjax: ', res);
     }); // closes high scores $http
-
-
   }, function onError(res){
     console.log('failjax: ', res);
   }); // closes machine id $http
 
-  // Allows for submission of high scores //
+
+  // POST NEW HIGH SCORE //
   vm.onSubmit = function(){
     console.log('high score submitted!');
-  }
+    console.log('nhs: ', vm.newHighScore);
 
+    vm.newHighScore = {}
 
+    $http({
+      method: 'POST',
+      url: 'http://pinballmap.com/api/v1/region/bayarea/machine_score_xrefs.json ',
+      data: vm.newHighScore,
+      headers: 'Access-Control-Allow-Origin'
+    }).then(function onSuccess(res){
+      console.log('nhs:', res.data);
+
+    }, function onError(res){
+      console.log('nhs failjax: ', res);
+    }); // closes high score 'POST' $http
+  } // closes function 'onSubmit'
 } // closes MachineShowController
+
+
+
+//http://pinballmap.com/api/v1/machine_score_xrefs.json
